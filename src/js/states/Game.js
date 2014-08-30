01f759e9284a80216a1741bdfc52fd046d7b66ae
Danny Arnold <despairblue@skeil.de>
2014-08-30 17:53:29 +0200

    :lipstick: Fix indent

--- src/js/states/Game.js
+++ src/js/states/Game.js
@@ -1,23 +1,22 @@
 // 'use strict';
 
-BasicGame.Game = function (game) {
-    this.game = game;
+BasicGame.Game = function(game) {
+  this.game = game;
 };
 
 BasicGame.Game.prototype = {
 
-	create: function () {
-        this.game.stage.backgroundColor = '#dddddd';
-        // Game init Code here.
-    },
+  create: function() {
+    this.game.stage.backgroundColor = '#dddddd';
+    // Game init Code here.
+  },
 
-	update: function () {
-	},
+  update: function() {},
 
-	quitGame: function (pointer) {
-        //	Stop music, delete sprites, purge caches, free resources, all that good stuff.
+  quitGame: function(pointer) {
+    //	Stop music, delete sprites, purge caches, free resources, all that good stuff.
 
-		//	Then let's go back to the main menu.
-		this.game.state.start('MainMenu');
-	}
+    //	Then let's go back to the main menu.
+    this.game.state.start('MainMenu');
+  }
 };
--- src/js/states/MainMenu.js
+++ src/js/states/MainMenu.js
@@ -1,25 +1,24 @@
 // 'use strict';
 
-BasicGame.MainMenu = function (game) {
-	this.game = game;
-	this.music = null;
-	this.playButton = null;
+BasicGame.MainMenu = function(game) {
+  this.game = game;
+  this.music = null;
+  this.playButton = null;
 };
 
 BasicGame.MainMenu.prototype = {
-	create: function () {
-        //this.music = this.add.audio('titleMusic');
-		//this.music.play();
+  create: function() {
+    //this.music = this.add.audio('titleMusic');
+    //this.music.play();
 
-		this.add.sprite(0, 0, 'titlepage');
-		this.playButton = this.add.button(200, 400, 'playButton', this.startGame, this, 'over', 'out', 'over');
-	},
+    this.add.sprite(0, 0, 'titlepage');
+    this.playButton = this.add.button(200, 400, 'playButton', this.startGame, this, 'over', 'out', 'over');
+  },
 
-	update: function () {
-	},
+  update: function() {},
 
-	startGame: function (pointer) {
-		//this.music.stop();
-		this.game.state.start('Game');
-	}
+  startGame: function(pointer) {
+    //this.music.stop();
+    this.game.state.start('Game');
+  }
 };
